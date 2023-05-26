9ef6f6e405e0ee5b01eb76bad1392cd498b9d1b8
Dan Scott <98423842+danscott1997@users.noreply.github.com>
2023-05-26 10:58:05 +0100

    Improve label download speed

--- resources/views/slots/pending-labels.blade.php
+++ resources/views/slots/pending-labels.blade.php
@@ -12,24 +12,24 @@
                 },
                 responseType: "blob",
             }).then((response) => {
-                let filename = response.headers['content-disposition'].split('filename=')[1].split('.')[0].replace(/['"]+/g, '');
-                let extension = response.headers['content-disposition'].split('.')[1].split(';')[0].replace(/['"]+/g, '');
-                const url = window.URL.createObjectURL(new Blob([response.data]));
-                const link = document.createElement("a");
-                link.href = url;
-                link.setAttribute("download", `${filename}.${extension}`);
-                document.body.appendChild(link);
-                link.click();
-                link.remove();
-
-                pauseLabelDownloader = 0;
+                if (response.headers['content-disposition'] !== undefined) {
+                    let filename = response.headers['content-disposition'].split('filename=')[1].split('.')[0].replace(/['"]+/g, '');
+                    let extension = response.headers['content-disposition'].split('.')[1].split(';')[0].replace(/['"]+/g, '');
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement("a");
+                    link.href = url;
+                    link.setAttribute("download", `${filename}.${extension}`);
+                    document.body.appendChild(link);
+                    link.click();
+                    link.remove();
+                }
             }).catch((error) => {
                 // Add 5 seconds after each fail
                 pauseLabelDownloader = pauseLabelDownloader + 5000;
             });
 
-            // Wait 3 seconds + the pause before getting more
-            await new Promise(resolve => setTimeout(resolve, 3000 + pauseLabelDownloader));
+            // Wait 1 second + the pause before getting more
+            await new Promise(resolve => setTimeout(resolve, 1000 + pauseLabelDownloader));
 
             downloadNextAvailableLabel();
         }
