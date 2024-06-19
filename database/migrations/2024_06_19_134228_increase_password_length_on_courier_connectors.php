3f9546eb1a84769be946c93696f5000900d07eaa
Chester Swann-Auger <chester@hey.com>
2024-06-19 14:03:46 +0100

    Fixes

--- database/migrations/2024_06_19_134228_increase_password_length_on_courier_connectors.php
+++ database/migrations/2024_06_19_134228_increase_password_length_on_courier_connectors.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class IncreasePasswordLengthForCourierConnectorsTable extends Migration
+class IncreasePasswordLengthOnCourierConnectors extends Migration
 {
     /**
      * Run the migrations.
